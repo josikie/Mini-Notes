1bfc442aedfcbef97e333db61b7e49b88046c7c7
Josi Kie <kiejosi12@gmail.com>
2023-02-16 14:39:59 +0700

    Add project files.

---
+++ Mini Notes/Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(593, 307)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(97, 32)
+        Me.Button4.TabIndex = 20
+        Me.Button4.Text = "Delete File"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(267, 380)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(63, 13)
+        Me.Label3.TabIndex = 19
+        Me.Label3.Text = "File Words: "
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(336, 377)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(65, 28)
+        Me.TextBox3.TabIndex = 18
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(313, 100)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(62, 13)
+        Me.Label2.TabIndex = 17
+        Me.Label2.Text = "Choose File"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(110, 129)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(466, 21)
+        Me.ComboBox1.TabIndex = 16
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(110, 52)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(466, 20)
+        Me.TextBox2.TabIndex = 15
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(593, 247)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(97, 32)
+        Me.Button3.TabIndex = 14
+        Me.Button3.Text = "Read From File"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(593, 185)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(97, 33)
+        Me.Button2.TabIndex = 13
+        Me.Button2.Text = "Write Into File"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(593, 46)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(97, 30)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = "Create New File"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(110, 185)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(466, 154)
+        Me.TextBox1.TabIndex = 11
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(790, 449)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Mini Notes"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Mini Notes/Form1.vb
@@ -0,0 +1,85 @@
+﻿Imports System.IO
+Imports System.Text
+
+Public Class Form1
+    Dim theFilePath As String = ""
+    Private Function countingWords(theText As String)
+        theText = " " + theText
+        theText = Replace(theText, vbNewLine, " ")
+        Dim numberOfWords As Integer = 0
+        For i = 1 To Len(theText) - 1
+            Dim firstSpace As String = Mid(theText, i, 1)
+            Dim lastSpace As String = Mid(theText, i + 1, 1)
+            If firstSpace = " " And lastSpace <> " " Then
+                numberOfWords += 1
+            End If
+        Next
+        Return numberOfWords
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim textTitle As String
+        Dim path As String = "C:\Users\LENOVO\source\repos\Mini Notes\Mini Notes\Files\"
+        textTitle = TextBox2.Text
+        path += textTitle
+        path += ".txt"
+
+        Dim idx As Integer
+        If ComboBox1.FindString(path) >= 0 Then
+            MessageBox.Show("Duplicate File Name")
+            idx = ComboBox1.FindString(path)
+        Else
+            idx = ComboBox1.Items.Add(path)
+        End If
+
+        ComboBox1.SelectedIndex = idx
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim text As New StringBuilder
+        text.AppendLine(TextBox1.Text)
+        Try
+            If theFilePath IsNot "" Then
+                System.IO.File.WriteAllText(theFilePath, text.ToString())
+                MessageBox.Show("Written to the file.")
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Invalid File")
+        End Try
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        theFilePath = ComboBox1.Text
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim path As String = "C:\Users\LENOVO\source\repos\Mini Notes\Mini Notes\Files\"
+        For Each file In My.Computer.FileSystem.GetFiles(path)
+            ComboBox1.Items.Add(file)
+        Next
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Try
+            If theFilePath IsNot "" Then
+                Dim theText As String = My.Computer.FileSystem.ReadAllText(theFilePath)
+                TextBox1.Text = theText
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Empty File")
+        End Try
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Dim numOfWords As Integer = countingWords(TextBox1.Text)
+        TextBox3.Text = numOfWords
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        File.Delete(theFilePath)
+        ComboBox1.Items.Remove(ComboBox1.SelectedItem)
+        ComboBox1.SelectedIndex = -1
+        MessageBox.Show("File Deleted.")
+    End Sub
+End Class
